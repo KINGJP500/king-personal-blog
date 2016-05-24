18d868a444f496a539dc616b807a6d0ca4b101b6
KING JP FLEURY <kingjpstar500@gmail.com>
2016-05-24 17:14:13 -0400

    renaming the post, work, posts . coffee to js and styling the home page and added some new code

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,12 +2,23 @@ $(window).scroll(function (){
     var wScroll = $(this).scrollTop();
 
     //The landing elements of the home page-scroll
-    if (wScroll > $('.home-work-pic').offset().top - ($(window).height() / 1.2)){
+    if (wScroll > $('.home_work_pic').offset().top - ($(window).height() / 1.2)){
 
-        $('.home-work-pic figure').each(function (i){
+        $('.home_work_pic figure').each(function (i){
 
             setTimeout(function(){
-                $('.home-work-pic figure').eq(i).addClass('is-showing');
+                $('.home_work_pic figure').eq(i).addClass('is-showing');
+            }, (700 * (Math.exp(i * 0.14))) - 700);
+        });
+    }
+
+    //The landing elements of the home page-scroll
+    if (wScroll > $('.b_home_work_pic').offset().top - ($(window).height() / 1.2)){
+
+        $('.b_home_work_pic figure').each(function (i){
+
+            setTimeout(function(){
+                $('.b_home_work_pic figure').eq(i).addClass('is-showing');
             }, (700 * (Math.exp(i * 0.14))) - 700);
         });
     }
