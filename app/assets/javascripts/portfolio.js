4f587186dbc1847fae25affea2df2120cd1bf7f4
KING JP FLEURY <kingjpstar500@gmail.com>
2016-06-02 23:04:01 -0400

    addes some png on the page add button to the figcaption

--- app/assets/javascripts/portfolio.js
+++ app/assets/javascripts/portfolio.js
@@ -1,13 +1,14 @@
 // create the elements for the portfolio-content
-window.onload = function any_function_name(){
-    var newHeading = document.createElement("h1");
-    var newParagrapth = document.createElement("p");
-    var menu = document.getElementById("portfolio-content");
-    menu.appendChild(newHeading);
-    menu.appendChild(newParagrapth);
-    newHeading.innerHTML = "Ruby on Rails";
-    newParagrapth.innerHTML = "Rails is a web framework for application";
-}
+// window.onload = function any_function_name(){
+//     var newHeading = document.createElement("h1");
+//     var newParagrapth = document.createElement("p");
+//     var menu = document.getElementById("portfolio-content");
+//     menu.appendChild(newHeading);
+//     menu.appendChild(newParagrapth);
+//     newHeading.innerHTML = "Ruby on Rails";
+//     newParagrapth.innerHTML = "Rails is a web framework for application";
+// }
+
 $(window).scroll(function(){
     var pScroll = $(this).scrollTop();
 
